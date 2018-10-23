46966bd8993d8c90b9e8b5e2b286d6bf3786de10
lorenzo.tonarelli9@gmail.com <michi01xy@gmail.com>
2018-10-23 10:30:15 +0200

    Massimo e minimo

---
+++ ProvaGit/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProvaGit
+{
+    public partial class Form1 : Form
+    {
+        List<int> vettore = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vettore.Add(Convert.ToInt32(numericUpDown1.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown2.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown3.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown4.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown5.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown6.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown7.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown8.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown9.Value));
+            vettore.Add(Convert.ToInt32(numericUpDown10.Value));
+            MessageBox.Show("Max: " + vettore.Max().ToString() + " Min: " + vettore.Min().ToString());
+            
+        }
+
+        
+    }
+}
